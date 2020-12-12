91612fc4416d20350148cc235731b3c42fdfde12
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2020-12-12 21:39:36 +0100

    Working app

---
+++ app/src/main/java/com/example/random/API.kt
@@ -0,0 +1,28 @@
+package com.example.random
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+const val API_KEY: String = "YOUR_API_KEY"
+
+interface Api {
+
+    @GET("movie/popular")
+    fun getPopularMovies(
+            @Query("api_key") apiKey: String = API_KEY,
+            @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovies(
+            @Query("api_key") apiKey: String = API_KEY,
+            @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+    @GET("movie/upcoming")
+    fun getUpcomingMovies(
+            @Query("api_key") apiKey: String = API_KEY,
+            @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+}
